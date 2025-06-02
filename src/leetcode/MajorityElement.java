d53033f39a0f2d2df638cff569f4c9cb9bd3efe2
shreeguruprasad <nsshreeguruprasad@gmail.com>
2025-06-02 23:40:40 +0530

    Day 2 : Majority problem

---
+++ src/leetcode/MajorityElement.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+
+	/*
+	 * Input: nums = [3,2,3] Output: 3
+	 * 
+	 * Input: nums = [2,2,1,1,1,2,2] Output: 2
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		int[] nums = {2,2,1,1,1,2,2,1,1,1,1};
+		int majority = 0;
+		int majorityValue=0;
+		Map<Integer,Integer> map = new HashMap<>();
+		for(int i=0;i<nums.length;i++) {
+			map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+			if(map.get(nums[i])>majority) {
+				majority=map.get(nums[i]);
+				majorityValue = nums[i];
+			}
+		}
+		
+		System.out.println("Highest repeated number is  : "+ majorityValue);
+	}
+
+}
