73da8090bbf875b8383a020d795861c73ea5275d
shreeguruprasad <nsshreeguruprasad@gmail.com>
2025-06-07 16:00:39 +0530

    formatting

--- src/leetcode/MaxNumInRightSideOfArray.java
+++ src/leetcode/MaxNumInRightSideOfArray.java
@@ -8,26 +8,25 @@ public class MaxNumInRightSideOfArray {
 	public static void main(String[] args) {
 		/*
 		 * 
-		 * Input: nums = [2,3,5,1,4]
-		 * Output: [5,4]
+		 * Input: nums = [2,3,5,1,4] Output: [5,4]
 		 * 
 		 */
-		
-		int[] nums = {2,3,5,1,4};
-		System.out.println("result :"+ getMaxNumberRightSideOfTheArray(nums).toString());
+
+		int[] nums = { 2, 3, 5, 1, 4 };
+		System.out.println("result :" + getMaxNumberRightSideOfTheArray(nums).toString());
 	}
 
 	private static List<Integer> getMaxNumberRightSideOfTheArray(int[] nums) {
 		int minimumNumber = Integer.MIN_VALUE;
 		List<Integer> list = new ArrayList();
-		
-		for(int i=nums.length-1;i>=0;i-- ) {
-			if(nums[i]>minimumNumber) {
+
+		for (int i = nums.length - 1; i >= 0; i--) {
+			if (nums[i] > minimumNumber) {
 				list.add(nums[i]);
-				minimumNumber=nums[i];
+				minimumNumber = nums[i];
 			}
 		}
-		
+
 		return list;
 	}
 
