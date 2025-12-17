e288b78b9fa39fe1d0665ee087bd94299c30b502
shreeguruprasad <nsshreeguruprasad@gmail.com>
2025-12-17 23:51:50 +0530

    practice

--- src/leetcode/RemoveDuplicateFromArrayII.java
+++ src/leetcode/RemoveDuplicateFromArrayII.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class RemoveDuplicateFromArrayII {
@@ -29,6 +30,24 @@ public class RemoveDuplicateFromArrayII {
 		
 		System.out.println("total number of turns to remove duplicate : "+count);
 		System.out.println(Arrays.toString(arr));
+		
+		// second time 
+		int[] inputArray= {0,0,1,1,1,1,2,2,3,3};
+		int countSecond=2;
+		
+		System.out.println(Arrays.toString(inputArray)+"  ==>>  ");
+		for(int i=2;i<inputArray.length;i++) {
+			
+			if(inputArray[i-2]!=inputArray[countSecond]) {
+				inputArray[countSecond] = inputArray[i];
+				countSecond++;
+			}
+			
+		}
+		
+		System.out.print(Arrays.toString(inputArray) +"       =>  "+countSecond);
+		
+		
 		
 	}
 
